e61e5197fa8493ee3c8c0322f4967087f654cae7
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2019-06-14 16:16:32 +0200

    Silencing warnings in rt_dmand.c

--- src/rt_dmand.c
+++ src/rt_dmand.c
@@ -84,7 +84,7 @@ void SD_UpdatePlaybackSound ( char ** ptr, unsigned long * length )
       return;
       }
 
-   *ptr=&PlaybackBuffer[PlayingPointer];
+   *ptr=(char*)&PlaybackBuffer[PlayingPointer];
 
    PlayingPointer = (PlayingPointer + *length) &
                       (PLAYBACKBUFFERSIZE - 1);
